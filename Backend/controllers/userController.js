48ddd5715d49ebfa0d3fea674f233ed5fa669976
Devesh Darshan Singh <sdevesh059@gmail.com>
2024-07-30 10:54:27 +0530

    Second commit

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,261 +1,111 @@
 import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
 import ErrorHandler from "../middlewares/error.js";
-import { Blog } from "../models/blogSchema.js";
+import { User } from "../models/userSchema.js";
+import { sendToken } from "../utils/jwtToken.js";
 import cloudinary from "cloudinary";
 
-export const blogPost = catchAsyncErrors(async (req, res, next) => {
+export const register = catchAsyncErrors(async (req, res, next) => {
   if (!req.files || Object.keys(req.files).length === 0) {
-    return next(new ErrorHandler("Blog Main Image Is Mandatory!", 400));
+    return next(new ErrorHandler("User Avatar Required!", 400));
   }
-  const { mainImage, paraOneImage, paraTwoImage, paraThreeImage } = req.files;
-  if (!mainImage) {
-    return next(new ErrorHandler("Blog Main Image Is Mandatory!", 400));
-  }
+  const { avatar } = req.files;
   const allowedFormats = ["image/png", "image/jpeg", "image/webp"];
-  if (
-    !allowedFormats.includes(mainImage.mimetype) ||
-    (paraOneImage && !allowedFormats.includes(paraOneImage.mimetype)) ||
-    (paraTwoImage && !allowedFormats.includes(paraTwoImage.mimetype)) ||
-    (paraThreeImage && !allowedFormats.includes(paraThreeImage.mimetype))
-  ) {
+  if (!allowedFormats.includes(avatar.mimetype)) {
     return next(
       new ErrorHandler(
-        "Invalid file type. Only JPG, PNG and WEBP Formats Are Allowed!",
+        "Invalid file type. Please provide your avatar in png, jpg or webp format.",
         400
       )
     );
   }
-  const {
-    title,
-    intro,
-    paraOneDescription,
-    paraOneTitle,
-    paraTwoDescription,
-    paraTwoTitle,
-    paraThreeDescription,
-    paraThreeTitle,
-    category,
-    published,
-  } = req.body;
-
-  const createdBy = req.user._id;
-  const authorName = req.user.name;
-  const authorAvatar = req.user.avatar.url;
-
-  if (!title || !category || !intro) {
-    return next(
-      new ErrorHandler("Title, Intro and Category Are Required Fields!", 400)
-    );
-  }
-
-  const uploadPromises = [
-    cloudinary.uploader.upload(mainImage.tempFilePath),
-    paraOneImage
-      ? cloudinary.uploader.upload(paraOneImage.tempFilePath)
-      : Promise.resolve(null),
-    paraTwoImage
-      ? cloudinary.uploader.upload(paraTwoImage.tempFilePath)
-      : Promise.resolve(null),
-    paraThreeImage
-      ? cloudinary.uploader.upload(paraThreeImage.tempFilePath)
-      : Promise.resolve(null),
-  ];
-
-  const [mainImageRes, paraOneImageRes, paraTwoImageRes, paraThreeImageRes] =
-    await Promise.all(uploadPromises);
-
+  const { name, email, password, phone, role, education } = req.body;
   if (
-    !mainImageRes ||
-    mainImageRes.error ||
-    (paraOneImage && (!paraOneImageRes || paraOneImageRes.error)) ||
-    (paraTwoImage && (!paraTwoImageRes || paraTwoImageRes.error)) ||
-    (paraThreeImage && (!paraThreeImageRes || paraThreeImageRes.error))
+    !name ||
+    !email ||
+    !password ||
+    !phone ||
+    !role ||
+    !education ||
+    !avatar
   ) {
-    return next(
-      new ErrorHandler("Error occured while uploading one or more images!", 500)
+    return next(new ErrorHandler("Please fill full details!", 400));
+  }
+  let user = await User.findOne({ email });
+  if (user) {
+    return next(new ErrorHandler("User already existes", 400));
+  }
+
+  const cloudinaryResponse = await cloudinary.uploader.upload(
+    avatar.tempFilePath
+  );
+  if (!cloudinaryResponse || cloudinaryResponse.error) {
+    console.error(
+      "Cloudinary error:",
+      cloudinaryResponse.error || "Unknown cloudinary error!"
     );
   }
-  const blogData = {
-    title,
-    intro,
-    paraOneDescription,
-    paraOneTitle,
-    paraTwoDescription,
-    paraTwoTitle,
-    paraThreeDescription,
-    paraThreeTitle,
-    category,
-    createdBy,
-    authorAvatar,
-    authorName,
-    published,
-    mainImage: {
-      public_id: mainImageRes.public_id,
-      url: mainImageRes.secure_url,
+
+  user = await User.create({
+    name,
+    email,
+    password,
+    phone,
+    role,
+    education,
+    avatar: {
+      public_id: cloudinaryResponse.public_id,
+      url: cloudinaryResponse.secure_url,
     },
-  };
-  if (paraOneImageRes) {
-    blogData.paraOneImage = {
-      public_id: paraOneImageRes.public_id,
-      url: paraOneImageRes.secure_url,
-    };
+  });
+  sendToken(user, 200, "User registered successfully", res);
+});
+
+export const login = catchAsyncErrors(async (req, res, next) => {
+  const { email, password, role } = req.body;
+  if (!email || !password || !role) {
+    return next(new ErrorHandler("Please fill full form!", 400));
   }
-  if (paraTwoImageRes) {
-    blogData.paraTwoImage = {
-      public_id: paraTwoImageRes.public_id,
-      url: paraTwoImageRes.secure_url,
-    };
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password!", 400));
   }
-  if (paraThreeImageRes) {
-    blogData.paraThreeImage = {
-      public_id: paraThreeImageRes.public_id,
-      url: paraThreeImageRes.secure_url,
-    };
+  const isPasswordMatched = await user.comparePassword(password);
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid email or password", 400));
   }
-  const blog = await Blog.create(blogData);
+  if (user.role !== role) {
+    return next(
+      new ErrorHandler(`User with provided role(${role}) not found`, 400)
+    );
+  }
+  sendToken(user, 200, "User logged in successfully", res);
+});
+
+export const logout = catchAsyncErrors((req, res, next) => {
+  res
+    .status(200)
+    .cookie("token", "", {
+      expires: new Date(Date.now()),
+      httpOnly: true,
+    })
+    .json({
+      success: true,
+      message: "User logged out!",
+    });
+});
+
+export const getMyProfile = catchAsyncErrors((req, res, next) => {
+  const user = req.user;
   res.status(200).json({
     success: true,
-    message: "Blog Uploaded!",
-    blog,
+    user,
   });
 });
 
-export const deleteBlog = catchAsyncErrors(async (req, res, next) => {
-  const { id } = req.params;
-  const blog = await Blog.findById(id);
-  if (!blog) {
-    return next(new ErrorHandler("Blog not found!", 404));
-  }
-  await blog.deleteOne();
+export const getAllAuthors = catchAsyncErrors(async (req, res, next) => {
+  const authors = await User.find({ role: "Author" });
   res.status(200).json({
     success: true,
-    message: "Blog deleted!",
-  });
-});
-
-export const getAllBlogs = catchAsyncErrors(async (req, res, next) => {
-  const allBlogs = await Blog.find({ published: true });
-  res.status(200).json({
-    success: true,
-    allBlogs,
-  });
-});
-
-export const getSingleBlog = catchAsyncErrors(async (req, res, next) => {
-  const { id } = req.params;
-  const blog = await Blog.findById(id);
-  if (!blog) {
-    return next(new ErrorHandler("Blog not found!", 404));
-  }
-  res.status(200).json({
-    success: true,
-    blog,
-  });
-});
-
-export const getMyBlogs = catchAsyncErrors(async (req, res, next) => {
-  const createdBy = req.user._id;
-  const blogs = await Blog.find({ createdBy });
-  res.status(200).json({
-    success: true,
-    blogs,
-  });
-});
-
-export const updateBlog = catchAsyncErrors(async (req, res, next) => {
-  const { id } = req.params;
-  let blog = await Blog.findById(id);
-  if (!blog) {
-    return next(new ErrorHandler("Blog not found!", 404));
-  }
-  const newBlogData = {
-    title: req.body.title,
-    intro: req.body.intro,
-    category: req.body.category,
-    paraOneTitle: req.body.paraOneTitle,
-    paraOneDescription: req.body.paraOneDescription,
-    paraTwoTitle: req.body.paraTwoTitle,
-    paraTwoDescription: req.body.paraTwoDescription,
-    paraThreeTitle: req.body.paraThreeTitle,
-    paraThreeDescription: req.body.paraThreeDescription,
-    published: req.body.published,
-  };
-  if (req.files) {
-    const { mainImage, paraOneImage, paraTwoImage, paraThreeImage } = req.files;
-    const allowedFormats = ["image/png", "image/jpeg", "image/webp"];
-    if (
-      (mainImage && !allowedFormats.includes(mainImage.mimetype)) ||
-      (paraOneImage && !allowedFormats.includes(paraOneImage.mimetype)) ||
-      (paraTwoImage && !allowedFormats.includes(paraTwoImage.mimetype)) ||
-      (paraThreeImage && !allowedFormats.includes(paraThreeImage.mimetype))
-    ) {
-      return next(
-        new ErrorHandler(
-          "Invalid file format. Only PNG, JPG and WEBp formats are allowed.",
-          400
-        )
-      );
-    }
-    if (req.files && mainImage) {
-      const blogMainImageId = blog.mainImage.public_id;
-      await cloudinary.uploader.destroy(blogMainImageId);
-      const newBlogMainImage = await cloudinary.uploader.upload(
-        mainImage.tempFilePath
-      );
-      newBlogData.mainImage = {
-        public_id: newBlogMainImage.public_id,
-        url: newBlogMainImage.secure_url,
-      };
-    }
-
-    if (req.files && paraOneImage) {
-      if (blog.paraOneImage && blog.paraOneImage.public_id) {
-        const blogParaOneImageId = blog.paraOneImage.public_id;
-        await cloudinary.uploader.destroy(blogParaOneImageId);
-      }
-      const newBlogParaOneImage = await cloudinary.uploader.upload(
-        paraOneImage.tempFilePath
-      );
-      newBlogData.paraOneImage = {
-        public_id: newBlogParaOneImage.public_id,
-        url: newBlogParaOneImage.secure_url,
-      };
-    }
-    if (req.files && paraTwoImage) {
-      if (blog.paraTwoImage && blog.paraTwoImage.public_id) {
-        const blogParaTwoImageId = blog.paraTwoImage.public_id;
-        await cloudinary.uploader.destroy(blogParaTwoImageId);
-      }
-      const newBlogParaTwoImage = await cloudinary.uploader.upload(
-        paraTwoImage.tempFilePath
-      );
-      newBlogData.paraTwoImage = {
-        public_id: newBlogParaTwoImage.public_id,
-        url: newBlogParaTwoImage.secure_url,
-      };
-    }
-    if (req.files && paraThreeImage) {
-      if (blog.paraThreeImage && blog.paraThreeImage.public_id) {
-        const blogParaThreeImageId = blog.paraThreeImage.public_id;
-        await cloudinary.uploader.destroy(blogParaThreeImageId);
-      }
-      const newBlogParaThreeImage = await cloudinary.uploader.upload(
-        paraThreeImage.tempFilePath
-      );
-      newBlogData.paraThreeImage = {
-        public_id: newBlogParaThreeImage.public_id,
-        url: newBlogParaThreeImage.secure_url,
-      };
-    }
-  }
-  blog = await Blog.findByIdAndUpdate(id, newBlogData, {
-    new: true,
-    runValidators: true,
-    useFindAndModify: false,
-  });
-  res.status(200).json({
-    success: true,
-    message: "Blog Updated!",
-    blog,
+    authors,
   });
 });
