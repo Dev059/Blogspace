0276514e17ae56724288505a4081952d1a6ddfd7
Devesh Darshan Singh <sdevesh059@gmail.com>
2025-04-11 15:41:02 +0530

    last

--- Frontend/src/components/pages/Home.jsx
+++ Frontend/src/components/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import { useContext} from "react";
 import LatestBlogs from "../miniComponents/LatestBlogs";
 import HeroSection from "../miniComponents/HeroSection";
 import TrendingBlogs from "../miniComponents/TrendingBlogs";
