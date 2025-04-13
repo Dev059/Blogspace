1862321321bbe38db4c04abc702abc277a9b9011
Devesh Darshan Singh <sdevesh059@gmail.com>
2025-04-13 15:06:03 +0530

    Final

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ const app = express();
 dotenv.config();
 
 app.use(cors({
-  origin: "https://your-frontend.vercel.app",
+  origin: "https://blogspace-xi.vercel.app",
   credentials: true,
 }));
 
