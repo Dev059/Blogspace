d992fbcfa6b9cb98a11f10072cb957121822573a
Devesh Darshan Singh <sdevesh059@gmail.com>
2025-04-13 19:45:03 +0530

    Final

--- Backend/utils/jwtToken.js
+++ Backend/utils/jwtToken.js
@@ -1,14 +1,19 @@
 import dotenv from "dotenv"
 dotenv.config()
 
-export const sendToken = (user, statusCode, message, res) => {
+
+  export const sendToken = (user, statusCode, message, res) => {
     const token = user.getJWTToken();
+  
     const options = {
       expires: new Date(
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
+      sameSite: "None", // Required when frontend and backend are on different domains
+      secure: true,     // Required when sameSite is None (HTTPS only)
     };
+  
     res.status(statusCode).cookie("token", token, options).json({
       success: true,
       user,
@@ -16,3 +21,4 @@ export const sendToken = (user, statusCode, message, res) => {
       token,
     });
   };
+  
